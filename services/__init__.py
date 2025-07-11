9d0dc3c7f618e4c699da16d9ce406c9866049a2d
fz0fz0fz <fz0fz0fz@gmail.com>
2025-07-11 04:50:24 +0300

    Update __init__.py

--- services/__init__.py
+++ services/__init__.py
@@ -1,5 +1,4 @@
-# من كل ملف خدمة handle استيراد دوال
-
+# من كل ملف خدمة استيراد دوال handle
 from .governmental import handle as governmental
 from .pharmacies import handle as pharmacies
 from .grocery import handle as grocery
@@ -11,35 +10,12 @@ from .restaurants import handle as restaurants
 from .stationery import handle as stationery
 from .shops import handle as shops
 from .chalets import handle as chalets
-from .water_truck import handle as water_truck
+from .water_truck import handle as water
 from .shovel import handle as shovel
 from .sand import handle as sand
 from .building_materials import handle as building_materials
 from .workers import handle as workers
 from .stores import handle as stores
 from .butchers import handle as butchers
-from .transport import handle as school
-from .alarm import handle as alarm
-
-__all__ = [
-    "governmental",
-    "pharmacies",
-    "grocery",
-    "vegetables",
-    "trips",
-    "desserts",
-    "home_businesses",
-    "restaurants",
-    "stationery",
-    "shops",
-    "chalets",
-    "water_truck",
-    "shovel",
-    "sand",
-    "building_materials",
-    "workers",
-    "stores",
-    "butchers",
-    "school",
-    "alarm"
-]
+from .transport import handle as school_transport
+from .reminder import handle as reminder  # تم التبديل من alarm إلى reminder
