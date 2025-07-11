d14657f51fc1b091953bb18ab50c8dca02c273dd
fz0fz0fz <fz0fz0fz@gmail.com>
2025-07-11 03:33:44 +0300

    Update __init__.py

--- services/__init__.py
+++ services/__init__.py
@@ -1,21 +1,45 @@
-# استيراد دوال handle من كل ملف خدمة
-from .governmental        import handle as governmental
-from .pharmacies         import handle as pharmacies
-from .grocery            import handle as grocery
-from .vegetables         import handle as vegetables
-from .trips              import handle as trips
-from .desserts           import handle as desserts
-from .home_businesses    import handle as home_businesses
-from .restaurants        import handle as restaurants
-from .stationery         import handle as stationery
-from .shops              import handle as shops
-from .chalets            import handle as chalets
-from .water_truck        import handle as water_truck
-from .shovel             import handle as shovel
-from .sand               import handle as sand
+# من كل ملف خدمة handle استيراد دوال
+
+from .governmental import handle as governmental
+from .pharmacies import handle as pharmacies
+from .grocery import handle as grocery
+from .vegetables import handle as vegetables
+from .trips import handle as trips
+from .desserts import handle as desserts
+from .home_businesses import handle as home_businesses
+from .restaurants import handle as restaurants
+from .stationery import handle as stationery
+from .shops import handle as shops
+from .chalets import handle as chalets
+from .water_truck import handle as water_truck
+from .shovel import handle as shovel
+from .sand import handle as sand
 from .building_materials import handle as building_materials
-from .workers            import handle as workers
-from .stores             import handle as stores
-from .butchers           import handle as butchers
-from .transport          import handle as school_transport
-from .alarm              import handle as alarm
+from .workers import handle as workers
+from .stores import handle as stores
+from .butchers import handle as butchers
+from .transport import handle as school
+from .alarm import handle as alarm
+
+__all__ = [
+    "governmental",
+    "pharmacies",
+    "grocery",
+    "vegetables",
+    "trips",
+    "desserts",
+    "home_businesses",
+    "restaurants",
+    "stationery",
+    "shops",
+    "chalets",
+    "water_truck",
+    "shovel",
+    "sand",
+    "building_materials",
+    "workers",
+    "stores",
+    "butchers",
+    "school",
+    "alarm"
+]
