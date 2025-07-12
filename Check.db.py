8654fd57f0c66902b335db71404d647a6d63807e
fz0fz0fz <fz0fz0fz@gmail.com>
2025-07-12 20:33:15 +0300

    Create Check.db.py

---
+++ Check.db.py
@@ -0,0 +1,8 @@
+import os
+
+db_path = os.path.join(os.getcwd(), "reminders.db")
+
+if os.path.exists(db_path):
+    print("✅ قاعدة البيانات reminders.db موجودة.")
+else:
+    print("❌ قاعدة البيانات reminders.db غير موجودة حتى الآن.")
