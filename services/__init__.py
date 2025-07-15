90316e59a5abdeb6d90acd6420f21569bbad84d2
fz0fz0fz <fz0fz0fz@gmail.com>
2025-07-16 01:17:50 +0300

    Update __init__.py

--- services/__init__.py
+++ services/__init__.py
@@ -1,4 +1,3 @@
-# services/__init__.py
 """
 This package contains service modules for the WhatsApp bot application.
 """
@@ -8,8 +7,7 @@ from .session import get_session, set_session, init_session_db
 from .reminder import handle as handle_reminder, init_reminder_db
 # خدمة بيانات الصيدليات والخدمات
 from .db import get_categories, init_db_and_insert_data
-# Connection Pooling
-from .db_pool import pool
+# Connection Pooling (no longer needed, removed pool import)
 
 __all__ = [
     "get_session",
@@ -19,5 +17,5 @@ __all__ = [
     "init_reminder_db",
     "get_categories",
     "init_db_and_insert_data",
-    "pool",
+    # Removed "pool" from __all__ since it's no longer used
 ]
